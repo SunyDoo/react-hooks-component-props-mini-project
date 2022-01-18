3f55a74cb1cdd1a25fcd305114811d4826ac4868
Sunil <snilavarath@gmail.com>
2022-01-18 14:45:31 -0500

    Done.

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,8 +2,8 @@ import React from "react";
 import Article from "./Article";
 
 
-function ArticleList({ articles }){
-    const articleObj = articles.map((article) => {
+function ArticleList({ posts }){
+    const articleObj = posts.map((article) => {
         return ( 
             <Article key = {article.id} title = {article.title} date = {article.date} preview ={article.preview} />
         )
